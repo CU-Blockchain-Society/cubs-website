ea75ceabf13f5eaec2b9d641fbc62b42e4956643
sprkzoff <sprkzoff@gmail.com>
2020-12-11 06:31:06 +0700

    :lipstick: finish sponsors & footer

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,12 @@ import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 import { Animated } from "react-animated-css"
 import Box from '@material-ui/core/Box'
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm'
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import BusinessIcon from '@material-ui/icons/Business'
 import Aos from "aos"
 import "aos/dist/aos.css"
+import FacebookIcon from '@material-ui/icons/Facebook'
+import FlagIcon from '@material-ui/icons/Flag'
 
 export default function Home(props) {
   const [presentPos, setPresentPos] = useState(0)
@@ -37,16 +39,16 @@ export default function Home(props) {
           </p>
         </Animated>
       </main>
-      <div className={styles.front_parallax} >
+      <div className={styles.content_zone} >
         <Box data-aos="fade-up" container className={styles.content_row} >
           <Box className={styles.content_col} >
             <div className={styles.paper}>
-              <AccessAlarmIcon style={{ fontSize: "10rem" }} />
+              <PeopleAltIcon style={{ fontSize: "10rem" }} />
             </div>
           </Box>
           <Box className={styles.content_col} >
             <div className={styles.paper}>
-              <h6 className={styles.subtitle} >Our goal</h6>
+              <h6 className={styles.subtitle} >Who are we?</h6>
               <p className={styles.content}>
                 Nulla minim dolore consectetur sit sit reprehenderit ex anim Minim aute cillu
               </p>
@@ -56,7 +58,7 @@ export default function Home(props) {
         <Box data-aos="fade-up" container className={styles.content_row} >
           <Box className={styles.content_col} >
             <div className={styles.paper}>
-              <BusinessIcon style={{ fontSize: "10rem" }} />
+              <FlagIcon style={{ fontSize: "10rem" }} />
             </div>
           </Box>
           <Box className={styles.content_col} >
@@ -69,8 +71,18 @@ export default function Home(props) {
           </Box>
         </Box>
       </div>
+      <div className={styles.sponsors_zone} >
+        <h6 className={styles.subtitle} >Supported by</h6>
+        <div className={styles.sponsors_wrap} >
+          <div className={styles.sponsor_logo} >
+            <img src="cuihub.png" alt="CU innovation hub" width="150" height="150"></img>
+          </div>
+        </div>
+      </div>
       <footer className={styles.footer}>
-
+        <h6 className={styles.contact_title} >Contact us</h6>
+        <p>contact@cublockchainsociety.com</p>
+        <a href="https://www.facebook.com/cubs2020" alt="cubs" ><FacebookIcon /> CU Blockchain Society</a>
       </footer>
     </div>
   )
