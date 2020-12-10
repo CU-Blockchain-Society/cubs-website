f6e9575fc0630e31bf4539b24fb16b31d3d5e399
sprkzoff <sprkzoff@gmail.com>
2020-12-11 02:11:04 +0700

    :lipstick: add content part

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 import {Animated} from "react-animated-css"
-import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box'
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm'
+import BusinessIcon from '@material-ui/icons/Business'
 
 export default function Home(props) {
   const [presentPos, setPresentPos] = useState(0)
@@ -21,41 +23,51 @@ export default function Home(props) {
       </Head>
       <Navbar />
       <main className={styles.main}>
-        <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDuration="2000" animationOutDuration="2000" >
+        <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDuration={2000} animationOutDuration={2000} >
           <h1 className={styles.title}>
             <span className={styles.CU_title}>CU</span> Blockchain Society
           </h1>
         </Animated>
-        <Animated animationIn="fadeInDownBig" animationInDelay="5000" animationInDuration="2000" animationOutDuration="2000" >
+        <Animated animationIn="fadeInDownBig" animationInDelay={5000} animationInDuration={2000} animationOutDuration={2000} >
           <p className={styles.description}>
             A blockchain community in Chulalongkorn University for the interested people
           </p>
         </Animated>
       </main>
       <div className={styles.front_parallax} >
-        <Grid container>
-          <Grid item xs={6}>
-            <div className={styles.paper}></div>
-          </Grid>
-          <Grid item xs={6}>
+        <Box container className={styles.content_row} >
+          <Box className={styles.content_col} >
+            <div className={styles.paper}>
+              <AccessAlarmIcon style={{ fontSize: "10rem" }} />
+            </div>
+          </Box>
+          <Box className={styles.content_col} >
             <div className={styles.paper}>
               <h6 className={styles.subtitle} >Our goal</h6>
-              <p className={styles.description}>
-                Nulla minim dolore consectetur sit sit reprehenderit ex anim. Minim aute cillu.
+              <p className={styles.content}>
+                Nulla minim dolore consectetur sit sit reprehenderit ex anim Minim aute cillu
               </p>
             </div>
-          </Grid>
-        </Grid>
+          </Box>
+        </Box>
+        <Box container className={styles.content_row} >
+          <Box className={styles.content_col} >
+            <div className={styles.paper}>
+              <BusinessIcon style={{ fontSize: "10rem" }} />
+            </div>
+          </Box>
+          <Box className={styles.content_col} >
+            <div className={styles.paper}>
+              <h6 className={styles.subtitle} >Our goal</h6>
+              <p className={styles.content}>
+                Nulla minim dolore consectetur sit sit reprehenderit ex anim Minim aute cillu
+              </p>
+            </div>
+          </Box>
+        </Box>
       </div>
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        
       </footer>
     </div>
   )
