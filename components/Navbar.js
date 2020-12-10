e6b908eb3907c10470db460793c5e96ecc978d31
sprkzoff <sprkzoff@gmail.com>
2020-12-11 06:30:02 +0700

    :building_construction: add about us

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,13 +3,13 @@ import Typography from '@material-ui/core/Typography'
 import Toolbar from '@material-ui/core/Toolbar'
 import { useEffect, useState } from 'react'
 import Slide from '@material-ui/core/Slide'
+import { Button, Grid } from '@material-ui/core'
 
 export default function Navbar(props) {
     const [prevPos, setPrevPos] = useState(0)
     const [presentPos, setPresentPos] = useState(0)
     useEffect(() => {
         setInterval(() => {
-            console.log(window.pageYOffset)
             setPrevPos(presentPos)
             setPresentPos(window.pageYOffset)
         }, 200)
@@ -18,7 +18,20 @@ export default function Navbar(props) {
         <Slide direction="down" in={!(presentPos > prevPos )}>
             <AppBar style={{ backgroundColor: `rgba(0,0,0,1)` }}>
                 <Toolbar>
-                    <Typography variant="h6">CUBS</Typography>
+                <Grid
+                    justify="space-between"
+                    container 
+                    spacing={24}
+                    alignItems="center"
+                >
+                    <a href="/" >
+                        <Typography variant="h6">CUBS</Typography>
+                    </a>
+                    <div>
+                        <Button color="inherit" href="/" >Home</Button>
+                        <Button color="inherit" href="/" >About us</Button>
+                    </div>
+                </Grid>
                 </Toolbar>
             </AppBar>
         </Slide>
