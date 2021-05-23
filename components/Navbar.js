48da87e361d0f17e84031ac492f6424d9985b8de
sprkzoff <sprkzoff@gmail.com>
2021-05-23 22:17:28 +0700

    update navbar (show when scroll down > 250)

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,9 @@
 import AppBar from '@material-ui/core/AppBar'
-import Typography from '@material-ui/core/Typography'
 import Toolbar from '@material-ui/core/Toolbar'
 import { useEffect, useState } from 'react'
 import Slide from '@material-ui/core/Slide'
 import { Button, Grid } from '@material-ui/core'
+import styles from '../styles/Navbar.module.css'
 
 export default function Navbar(props) {
     const [prevPos, setPrevPos] = useState(0)
@@ -15,7 +15,7 @@ export default function Navbar(props) {
         }, 200)
     }, [])
     return (
-        <Slide direction="down" in={!(presentPos > prevPos )}>
+        <Slide direction="down" in={ presentPos >= 250 }>
             <AppBar style={{ backgroundColor: `rgba(0,0,0,1)` }}>
                 <Toolbar>
                 <Grid
@@ -24,12 +24,14 @@ export default function Navbar(props) {
                     spacing={24}
                     alignItems="center"
                 >
-                    <a href="/" >
+                    <a className={styles.wrap} href="/" >
                         <img src="cubs_nav.jpg" alt="CUBS" height="35"></img>
+                        <p className={styles.home_text} >CUBlockchainSociety</p>
                     </a>
                     <div>
                         <Button color="inherit" href="/" >Home</Button>
                         <Button color="inherit" href="/about" >About us</Button>
+                        <Button color="inherit" href="/roadmap" >Events</Button>
                     </div>
                 </Grid>
                 </Toolbar>
